95089b82e73c587811fc20642f0a2c64f14d0796
ptdat97 <ptdats2@gmail.com>
2026-06-28 13:32:18 +0700

    css

--- themes/fashion/views/partials/header.blade.php
+++ themes/fashion/views/partials/header.blade.php
@@ -5,7 +5,7 @@
     </div>
 @endif
 
-<header class="site-header border-bottom">
+<header class="site-header">
     <nav class="navbar navbar-expand-lg bg-white">
         <div class="container">
             {{-- Mobile: hamburger toggles the off-canvas menu --}}
